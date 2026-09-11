a6794d32f95074bc896a23be9efad2d8cd8ac69d
lathikareddychagamreddy-ops <lathikareddychagamreddy@gmail.com>
2026-09-11 13:37:06 +0530

    Init. Commit

--- my-project/client/vite.config.js
+++ my-project/client/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base: './' // Ensures styles and icons load on GitHub Pages
 });
